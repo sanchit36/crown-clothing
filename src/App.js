4b0cdfd7fcb2bed9a6b86e6a64908f487faea40c
sanchit36 <sanchitbhadgal36@gmail.com>
2021-03-23 11:13:01 +0530

    added checkout page, checkout-item component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckOut from "./pages/checkout/checkout.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
@@ -46,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckOut} />
           <Route
             path="/signin"
             render={() =>
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -21,3 +21,11 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumaletedQuantity, cartItem) =>
+      accumaletedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
+);
