ddbe681ed8decb4c2ff9c9b4749b5b7a00a23dca
snachit36 <sbhadgal_be19@thapar.edu>
2021-04-16 14:22:44 +0530

    separating app into client folder, adding express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
 import { clearCart } from "../../redux/cart/cart.actions";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price, dispatch }) => {
   const priceForStripe = price * 100;
@@ -9,7 +10,23 @@ const StripeCheckoutButton = ({ price, dispatch }) => {
     "pk_test_51HLLbUIC8Pm5McXxkTTh9vhvJN4UPjPQXTD6ZvkcrHNzM6zpzrF5iDstX5kx2FjPbCSWnyZUZzE9Yx4nBKGRYsAl00yK2hUQZY";
   const onToken = (token) => {
     dispatch(clearCart());
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successfull");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", error);
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
@@ -24,6 +41,7 @@ const StripeCheckoutButton = ({ price, dispatch }) => {
       panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
+      currency="inr"
     />
   );
 };
